7914446fc0e33af313c0ac4e7c4f87207cd5fce9
Sand Yang <sand050965@gmail.com>
2023-08-04 19:45:35 +0800

    style: Modified style for navigation drawer

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -6,11 +6,12 @@ import {useRailStore} from "@/stores/rail";
 const drawerStore = useDrawerStore();
 
 const railStore = useRailStore();
+
 </script>
 
 <template>
   <v-navigation-drawer
-      class="bg-teal "
+      class="bg-teal navigation-drawer"
       v-model="drawerStore.drawer"
       :rail="railStore.rail"
       permanent
@@ -18,75 +19,121 @@ const railStore = useRailStore();
     <v-list-item
         prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
         title="John Leider"
-        nav
     >
     </v-list-item>
 
-    <v-divider class="my-2"></v-divider>
+    <v-divider></v-divider>
 
     <v-list
-        class="pr-0"
-        nav
+        class="pr-0 pt-8"
+        theme="light"
     >
-      <v-hover v-slot="{ isHovering, props }">
-        <v-list-item
-            :class="{ 'on-hover': isHovering }"
-            v-bind="props"
-            prepend-icon="fa:fas fa-tachometer-alt"
-            title="Dashboard"
-            to="/dashboard"
-        ></v-list-item>
-      </v-hover>
+      <v-list-item
+          v-bind="props"
+          prepend-icon="fa:fas fa-tachometer-alt"
+          to="/dashboard"
+          :ripple="{ class: 'ripple-expand' }"
+          variant="text"
+      >
+        <h3>
+          Dashboard
+        </h3>
+      </v-list-item>
 
-      <v-hover v-slot="{ isHovering, props }">
-        <v-list-item
-            :class="{ 'on-hover': isHovering }"
-            v-bind="props"
-            prepend-icon="fa:fas fa-users"
-            title="Users Management"
-            to="/user"
-        ></v-list-item>
-      </v-hover>
+      <v-list-item
+          v-bind="props"
+          prepend-icon="fa:fas fa-users"
+          to="/user"
+          :ripple="true"
+      >
+        <h3>
+          Users Management
+        </h3>
+      </v-list-item>
 
-      <v-hover v-slot="{ isHovering, props }">
-        <v-list-item
-            :class="{ 'on-hover': isHovering }"
-            v-bind="props"
-            prepend-icon="fa:fas fa-newspaper"
-            title="Posts Management"
-            to="/post"
-        ></v-list-item>
-      </v-hover>
+      <v-list-item
+          v-bind="props"
+          prepend-icon="fa:fas fa-paste"
+          to="/post"
+          :ripple="true"
+      >
+        <h3>
+          Posts Management
+        </h3>
+      </v-list-item>
 
-      <v-hover v-slot="{ isHovering, props }">
-        <v-list-item
-            :class="{ 'on-hover': isHovering }"
-            v-bind="props"
-            prepend-icon="fa:fas fa-tags"
-            title="Tags Management"
-            to="/tag"
-        ></v-list-item>
-      </v-hover>
+      <v-list-item
+          v-bind="props"
+          prepend-icon="fa:fas fa-tags"
+          to="/tag"
+          :ripple="true"
+      >
+        <h3>
+          Tags Management
+        </h3>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <style scoped lang="scss">
-.v-list-item:is(.on-hover) {
+.navigation-drawer {
+  border: 0;
+}
+
+.v-list-item {
+  height: 60px;
+  padding: 10px;
+  margin: 50px 0;
+  line-height: 60px;
+  color: var(--white);
+  text-align: start;
+  white-space: nowrap;
+}
+
+.v-list-item::after {
+  transition: none;
+}
+
+.v-list-item:hover,
+.v-list-item--active {
   color: var(--green);
   background-color: var(--white);
   border-top-left-radius: 30px;
   border-bottom-left-radius: 30px;
+  transition: 0.1s ease-in;
 }
 
-.v-list-item::before:is(.on-hover) {
+.v-list-item:hover::before,
+.v-list-item--active::before {
   content: '';
   position: absolute;
   right: 0;
-  
+  top: -50px;
+  width: 50px;
+  height: 50px;
+  background-color: transparent;
+  border-radius: 50%;
+  box-shadow: 35px 35px 0 10px var(--white);
+  pointer-events: none;
 }
 
-.v-list-item:not(.on-hover) {
-  transition: opacity .4s ease-in-out;
+.v-list-item:hover::after,
+.v-list-item--active::after {
+  content: '';
+  position: absolute;
+  right: 0;
+  left: calc(100% - 50px);
+  top: 100%;
+  bottom: -50px;
+  width: 50px;
+  height: 50px;
+  background-color: transparent;
+  border-radius: 50%;
+  box-shadow: 35px -35px 0 10px var(--white);
+  pointer-events: none;
+  opacity: 1;
+  border: 0;
+  z-index: -1;
 }
 </style>
